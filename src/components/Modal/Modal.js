0708b85cf470c44ce161fcd26abdd7cb55e391f7
guipure <igor778868@gmail.com>
2020-06-07 22:20:59 +0300

    Fixed overflow properties of modal window

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,11 +3,17 @@ import './Modal.css'
 
 
 export default function Modal({ children, onclose }) {
+  document.body.style.overflow = 'hidden'
+  const closeModal = function() {
+    onclose()
+    document.body.style.overflow = 'auto'
+  }
+
   return (
     <div className="modal">
       <div className="modal-body">
         {children}
-        <button type="button" className="modal__close-btn" onClick={onclose}>&times;</button>
+        <button type="button" className="modal__close-btn" onClick={closeModal}>&times;</button>
       </div>
     </div>
   )
