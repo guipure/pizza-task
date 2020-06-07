de7836ba3a637c7da372a550dbabeece21c54a29
guipure <igor778868@gmail.com>
2020-06-07 19:08:19 +0300

    API service is changed to more reliable

--- src/converter.js
+++ src/converter.js
@@ -1,5 +1,5 @@
 export default async function convertEURtoUSD(eur) {
-  const url = 'http://data.fixer.io/api/latest?access_key=565a8176bdb3ec80eadb1fe0469a3097&symbols=USD'
+  const url = 'https://api.exchangeratesapi.io/latest'
   const response = await fetch(url)
   const json = await response.json()
   const USD = Math.round(eur * json.rates.USD * 100) / 100
