fd4a3dc19b28202857477de4aec17c86be937ab9
guipure <igor778868@gmail.com>
2020-06-07 11:30:20 +0300

    Modal component refactoring

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,25 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
 import './Modal.css'
 
-export default function Modal({ children, onclose }) {
-  const modalRoot = document.getElementById('modal-root')
 
-  // We should close previous modal window if it exists
-  const prevModal = modalRoot.querySelector('.modal')
-  if (prevModal) modalRoot.removeChild(prevModal)
-  
-  const modalWindow = (
+export default function Modal({ children, onclose }) {
+  return (
     <div className="modal">
       <div className="modal-body">
         {children}
         <button type="button" className="modal__close-btn" onClick={onclose}>&times;</button>
       </div>
     </div>
-  )
-  
-  return ReactDOM.createPortal(
-    modalWindow,
-    modalRoot
   )
 }
