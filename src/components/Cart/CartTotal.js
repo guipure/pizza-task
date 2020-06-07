e40715c9b5593c13cd3ea4255359b6a3c92506fd
guipure <igor778868@gmail.com>
2020-06-07 19:06:55 +0300

    Added delivery cost

--- src/components/Cart/CartTotal.js
+++ src/components/Cart/CartTotal.js
@@ -6,29 +6,34 @@ import convertEURtoUSD from '../../converter'
 export default function CartTotal() {
   const { cart } = useContext(Context)
   const [USD, setUSD] = useState('$')
-  const EUR = calcTotal(cart)
-  convertEURtoUSD(
-    Number.parseFloat(EUR))
-    .then(res => setUSD(res + ' $')
-  )
+  const deliveryCost = 5
+  const EURnum = calcTotal(cart, deliveryCost)
+  const EUR = EURnum.toString() + ' €'
+  const estimatedUSD = Math.round(EURnum * 1.13 * 100) / 100 + ' $'
+  convertEURtoUSD(EURnum)
+    .then(res => setUSD(res + ' $'), err => setUSD(estimatedUSD))
 
-  function calcTotal(cart) {
-    let sum = 0
+  function calcTotal(cart, deliveryCost) {
+    let sum = deliveryCost
 
     for (let item of cart) {
       sum += item.quantity * item.price
     }
 
-    return sum.toString() + ' €'
+    const result = Math.round(sum * 100) / 100
+    return result
   }
 
   
   return (
     <div className="cart-total">
-      <h3 className="cart-total__label">Total price:</h3>
-      <div className="cart-total__price">
-        <h3 className="cart-total__eur">{EUR}</h3>
-        <h3 className="cart-total__usd">{USD}</h3>
+      <p className="cart-total__delivery">+ 5 € delivery cost</p>
+      <div className="cart-total__main">
+        <h3 className="cart-total__label">Total:</h3>
+        <div className="cart-total__price">
+          <h3 className="cart-total__eur">{EUR}</h3>
+          <h3 className="cart-total__usd">{USD}</h3>
+        </div>
       </div>
     </div> 
   )
